04ff2d5bb1fd2e5b5c8404ac8960a4b49fa7c913
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-09-11 00:16:47 -0300

    Add Comment

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { View, Image, StyleSheet, Dimensions } from 'react-native'
+import AddComment from './AddComment'
 import Author from './Author'
+import Comments from './Comments'
 
 export default props =>
     <View style={styles.container}>
         <Image source={props.image} style={styles.image}/>
         <Author email='wcaquino@gmail.com' nickname='Wagner' />
+        <Comments comments={props.comments} />
+        <AddComment />
     </View>
 
 const styles = StyleSheet.create({
