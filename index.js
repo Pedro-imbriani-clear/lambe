f4158a9d4d87e28fcac3d625fde22e9f48ea0f2d
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-09-12 23:31:33 -0300

    AuthOrProfile

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Navigator);
